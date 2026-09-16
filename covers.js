d29a359ccf7b68972be2276dde719be73e7dd737
Дмитрий  Дружков <dmitrijdruzkov@MacBook-Pro-Dmitrij.local>
2026-09-16 15:51:53 +0500

    Обновление материалов уроков

--- covers.js
+++ covers.js
@@ -3,6 +3,7 @@
 var COVERS = {
   "geometriya-1": "covers/geometriya-1.webp",
   "geometriya-2": "covers/geometriya-2.webp",
+  "geometriya-3": "covers/geometriya-3.webp",
   "algebra-1": "covers/algebra-1.webp",
   "algebra-2": "covers/algebra-2.webp",
   "algebra-3": "covers/algebra-3.webp",
