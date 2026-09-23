8dd2468645643197e72333b9184d91dac6ee36a8
Дмитрий  Дружков <dmitrijdruzkov@MacBook-Pro-Dmitrij.local>
2026-09-23 16:31:45 +0500

    Обновление материалов уроков

--- covers.js
+++ covers.js
@@ -4,12 +4,15 @@ var COVERS = {
   "geometriya-1": "covers/geometriya-1.webp",
   "geometriya-2": "covers/geometriya-2.webp",
   "geometriya-3": "covers/geometriya-3.webp",
+  "geometriya-4": "covers/geometriya-4.webp",
   "algebra-1": "covers/algebra-1.webp",
   "algebra-2": "covers/algebra-2.webp",
   "algebra-3": "covers/algebra-3.webp",
   "algebra-4": "covers/algebra-4.webp",
   "algebra-5": "covers/algebra-5.webp",
   "algebra-6": "covers/algebra-6.webp",
+  "algebra-7": "covers/algebra-7.webp",
   "veroyatnost-1": "covers/veroyatnost-1.webp",
-  "veroyatnost-2": "covers/veroyatnost-2.webp"
+  "veroyatnost-2": "covers/veroyatnost-2.webp",
+  "veroyatnost-3": "covers/veroyatnost-3.webp"
 };
